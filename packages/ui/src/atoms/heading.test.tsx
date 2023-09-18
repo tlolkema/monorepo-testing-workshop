fe6f2656655633f4603df7b21d402575fc542192
Tim Lolkema <timlolkema@gmail.com>
2023-09-18 23:12:58 +0200

    feat: add vitest

---
+++ packages/ui/src/atoms/heading.test.tsx
@@ -0,0 +1,9 @@
+import { expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Heading } from "./heading";
+
+it("renders heading", () => {
+  render(<Heading title="Hello" />);
+  const headline = screen.getByText(/Hello/i);
+  expect(headline).not.toBeNull();
+});
