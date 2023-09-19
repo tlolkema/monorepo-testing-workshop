62c8f5d65839a1c6494d58b25c34d44ac27e8d9f
Tim Lolkema <timlolkema@gmail.com>
2023-09-19 22:41:11 +0200

    test: add pipeline

--- apps/pizza-shop/playwright.config.ts
+++ apps/pizza-shop/playwright.config.ts
@@ -7,25 +7,22 @@ export default defineConfig({
   use: {
     baseURL: process.env.PLAYWRIGHT_BASE_URL
       ? process.env.PLAYWRIGHT_BASE_URL
-      : "http://127.0.0.1:3000",
+      : "http://127.0.0.1:3001",
     trace: "on-first-retry",
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
   ],
 });
