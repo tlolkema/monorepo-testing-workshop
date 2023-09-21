efb60e56e249775a08bba2f520f7df02fed299cd
Bastian Nijboer <bastiannijboer@gmail.com>
2023-09-21 20:37:04 +0200

    Revert "changes"
    This reverts commit 3a154c4b3f3ac5c426be8292bab0e1e909c445d2.

--- apps/test-automation/src/app/contact/page.tsx
+++ apps/test-automation/src/app/contact/page.tsx
@@ -1,10 +1,16 @@
-import { Heading, Button } from "ui";
+import { Heading } from "ui";
 
 export default function Page(): JSX.Element {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Heading title="Contact Us" />
-      <Button title="Click me" />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        data-testid="contact-button"
+        type="button"
+      >
+        Click me
+      </button>
     </main>
   );
 }
--- apps/test-automation/tests/seo.spec.ts
+++ apps/test-automation/tests/seo.spec.ts
@@ -1,6 +1,3 @@
 import { shouldHavePageTitle } from "e2e-tests/tests/seo";
-import { navigateToLinkAndCheckTitle } from "e2e-tests/tests/seo";
 
 shouldHavePageTitle({ pageTitle: "Test Automation" });
-
-navigateToLinkAndCheckTitle({ linkName: "Contact Us!", pageTitle: "Contact Us"});
