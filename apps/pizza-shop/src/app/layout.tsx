6d926ba48533fc12bd8c8e5446f6313b557bce99
Martyn de Vries <vriesdemartyn@gmail.com>
2023-09-21 20:28:39 +0200

    Changed something

--- apps/pizza-shop/src/app/layout.tsx
+++ apps/pizza-shop/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang="en">
+    <html lang="nl">
       <body className={inter.className}>{children}</body>
     </html>
   );
